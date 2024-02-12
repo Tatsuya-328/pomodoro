3a1c7394902de1000e50aa15d88300b7702830fc
Tatsuya-328 <kawanou328@yahoo.co.jp>
2024-02-12 15:30:00 +0900

    fix: 依存関係

--- src/App.js
+++ src/App.js
@@ -53,13 +53,13 @@ const App = () => {
     }
   }, [phase, workMaxSeconds, breakMaxSeconds]);
 
-  const playSound = () => {
-    if (isSoundOn) {
-      const audio = new Audio(audio1);
-      audio.play();
-    }
-  };
   useEffect(() => {
+    const playSound = () => {
+      if (isSoundOn) {
+        const audio = new Audio(audio1);
+        audio.play();
+      }
+    };
     if (seconds === 0) {
       playSound();
     }
